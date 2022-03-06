caf4de407f877ce1972d2a0b9349f80c14b55fa2
Your Name <amr7hmed@gmail.com>
2022-03-06 16:50:49 +0200

    Add Home Page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,59 @@
-
-var owl = $('.owl-developers');
-owl.owlCarousel({
-    loop:true,
-    nav:true,
-    margin:10,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },            
-        960:{
-            items:5
-        },
-        1200:{
-            items:8
-        }
-    }
+var owldeveloper = $(".owl-developer");
+owldeveloper.owlCarousel({
+  loop: false,
+  margin: 15,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    960: {
+      items: 5,
+    },
+    1200: {
+      items: 8,
+    },
+  },
 });
-owl.on('mousewheel', '.owl-stage', function (e) {
-    if (e.deltaY>0) {
-        owl.trigger('next.owl');
-    } else {
-        owl.trigger('prev.owl');
-    }
-    e.preventDefault();
+
+// Custom Button
+$(".NextBtndeveloper").click(function () {
+    owldeveloper.trigger("next.owl.carousel");
+});
+$(".PreviousBtndeveloper").click(function () {
+    owldeveloper.trigger("prev.owl.carousel");
+});
+
+
+
+var owlproject = $(".latest_project");
+owlproject.owlCarousel({
+  loop: false,
+  margin: 30,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    960: {
+      items: 2,
+    },
+    1200: {
+      items: 3,
+    },
+  },
+});
+
+// Custom Button
+$(".NextBtnproject").click(function () {
+    owlproject.trigger("next.owl.carousel");
+});
+$(".PreviousBtnproject").click(function () {
+    owlproject.trigger("prev.owl.carousel");
 });
