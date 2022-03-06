1ed2de62a7f03f18f420a0fcff47c27bcb0f1969
Amr7hmed <amr7hmed@gmail.com>
2022-03-06 06:34:43 +0200

    Update Project

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+
+var owl = $('.owl-developers');
+owl.owlCarousel({
+    loop:true,
+    nav:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },            
+        960:{
+            items:5
+        },
+        1200:{
+            items:8
+        }
+    }
+});
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
